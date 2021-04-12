46efc7d49c3eeafdc4d2d6b5c16b89683ce3fe4b
pasjkeee <pasjkeee@gmail.com>
2021-04-12 18:09:34 +0300

    CalContainer component Class to FC refactoring

--- dip/src/components/calendar/cal-container/calContainer.js
+++ dip/src/components/calendar/cal-container/calContainer.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import WithRestoService from '../../hoc';
 import styled from 'styled-components';
 import {nextMonth, prevMonth} from '../../../actions' 
 
@@ -58,50 +57,29 @@ const BackImgWrapper = styled.img`
     right: 40px;
     opacity: .5;
 `
-
-const MyDate = styled.span`
-
-`
-
 const months = ["Январь", "Февраль", "Март" ,"Апрель", "Май", "Июнь", "Июль" ,"Август" ,"Сентябрь" ,"Октябрь", "Ноябрь","Декабрь"];
-const digits = ["01", "02", "03" ,"04", "05", "06", "07" ,"08" ,"09" ,"10", "11","12"];
 
+const CalContainer = (props) => {
 
-class CalContainer extends React.Component{
-
-    constructor(){
-        super();
-        this.state = 
-            {
-                month: 0
-            }
-        
-    }
-
-    render(){
-
-
-        return(
-            <CalContainerDiv>
-                    <Mon>
-                        <ArrL>
-                            <ArrImg src={arrL}
-                            onClick={()=>{
-                                this.props.prevMonth()}}/>
-                        </ArrL>
-                        <MyDate>
-                            {`${months[this.props.envMonth]} ${this.props.envYear}`}
-                        </MyDate>
-                        <ArrR>
-                            <ArrImg src={arrR}
-                            onClick={()=>{this.props.nextMonth()}}/>
-                        </ArrR>
-                    </Mon>
-                    <CalContainerItem month={this.props.envMonth}/>
-                    <BackImgWrapper src={BgImg}/>
-            </CalContainerDiv>
-        )
-    }
+    return(
+        <CalContainerDiv>
+                <Mon>
+                    <ArrL>
+                        <ArrImg src={arrL}
+                        onClick={()=>{props.prevMonth()}}/>
+                    </ArrL>
+                    <div>
+                        {`${months[props.envMonth]} ${props.envYear}`}
+                    </div>
+                    <ArrR>
+                        <ArrImg src={arrR}
+                        onClick={()=>{props.nextMonth()}}/>
+                    </ArrR>
+                </Mon>
+                <CalContainerItem month={props.envMonth}/>
+                <BackImgWrapper src={BgImg}/>
+        </CalContainerDiv>
+    )
 }
 
 const mapStateToProps = (state) => {
